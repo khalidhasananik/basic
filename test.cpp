b2c9549a2bcc813e1ed50047b5b8dbcecd1b8f79
Khalid Hasan <khalid.hasan1@northsouth.edu>
2023-05-08 20:35:07 +0600

    test

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char **argv)
+{
+
+    cout << "this is a test file for github actions" << endl;
+
+    cout << "this is a test file for github actions" << endl;
+
+    return 0;
+}
